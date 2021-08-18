7cf0f9570cdf1627c1253b563a85fb5f64b60cec
Nate Bruggeman <njbruggeman@gmail.com>
2021-08-18 11:19:37 -0500

    added callbacks and error handling

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const http = new easyHTTP;
 
 
 //GET posts
-http.get('https://jsonplaceholder.typicode.com/posts');
+http.get('https://jsonplaceholder.typicode.com/posts', 
+function(err, posts){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(posts);
+    }
+});
 
 
 
--- easyhttp.js
+++ easyhttp.js
@@ -4,13 +4,15 @@ function easyHTTP() {
 
 // make a HTTP GET request
 
-easyHTTP.prototype.get = function(url) {
+easyHTTP.prototype.get = function(url, callback) {
     this.http.open('GET', url, true);
 
     let self = this;
     this.http.onload = function() {
         if(self.http.status === 200){
-            console.log(self.http.responseText)
+            callback(null, self.http.responseText)
+        } else {
+            callback('Error: ' + self.http.status);
         }
     }
 
